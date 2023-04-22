1e583b17ae2b030ce75c02fc5a342dde7922358b
ashutosh030103 <131470507+ashutosh030103@users.noreply.github.com>
2023-04-22 17:14:40 +0530

    Add files via upload

---
+++ My SQL Data/Data Analysis Project.sql
@@ -0,0 +1,13 @@
+create schema hotel_raddison;
+
+use hotel_raddison;
+
+select * from date
+
+select * from hotels
+
+select * from rooms
+
+select * from fact_aggregated_bookings
+
+select * from fact_bookings
